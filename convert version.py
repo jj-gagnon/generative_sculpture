57dfa001ed5a999d1a2b54c5ec7773290bd8a8c3
jj-gagnon <essejnhojnongag@gmail.com>
2025-12-09 23:27:40 -0500

    deleted fiels

--- convert version.py
+++
@@ -1,7 +0,0 @@
-
-from ezdxf import recover
-
-# Use the recover module to load the DXF files, the input files may not be error free:
-doc, _ = recover.readfile("5 nested 46 by 12_5.dxf")
-doc.dxfversion = "R2018"
-doc.saveas("R2018.dxf")
