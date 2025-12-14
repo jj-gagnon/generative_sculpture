8a24c068f806d5280f28b11c3b30b54a1aad992b
jj-gagnon <essejnhojnongag@gmail.com>
2025-12-14 16:48:38 -0500

    Update main.js

--- main.js
+++ main.js
@@ -45,7 +45,7 @@ function main() {
     let w = 1080 + 650
     let h = 1920 - 100
     let scale = 0.5
-    renderer.setSize(w * scale, h * scale, true)
+    // renderer.setSize(w * scale, h * scale, true)
 
 
 
